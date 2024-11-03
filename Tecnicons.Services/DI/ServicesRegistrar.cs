af745d9939b2e870757d9c03475304c7ea398dfe
Alma Ozoria <heisylior@hotmail.com>
2024-11-02 20:43:57 -0400

    Creando services registrar de la carpecta Di del class library tecnicos services

---
+++ Tecnicons.Services/DI/ServicesRegistrar.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tecnicos.Data.DI;
+using Tecnicos.Abstractions;
+
+namespace Tecnicos.Services.DI;
+
+public static class ServicesRegistrar
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.RegisterDbContextFactory(configuration);
+        services.AddScoped<IClienteService, ClienteService>();
+        return services;
+    }
+}
