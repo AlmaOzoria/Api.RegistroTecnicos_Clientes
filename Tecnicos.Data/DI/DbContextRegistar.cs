2aa061b6c1485913a955e5dea9809a687eddecce
Alma Ozoria <heisylior@hotmail.com>
2024-11-02 20:41:55 -0400

    Creando Dbcontextregistrar de la carpeta DI

---
+++ Tecnicos.Data/DI/DbContextRegistar.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tecnicos.Data.Context;
+
+namespace Tecnicos.Data.DI;
+
+public static class DbContextRegistar
+{
+    public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextFactory<TecnicosContext>(o =>
+            o.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
+                sqlOptions.EnableRetryOnFailure()));
+
+        return services;
+    }
+}
