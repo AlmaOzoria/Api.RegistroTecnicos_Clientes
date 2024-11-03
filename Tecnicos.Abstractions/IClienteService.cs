20bfad0ed5269b8a2e0a0700e01b69dc862283a4
Alma Ozoria <heisylior@hotmail.com>
2024-11-02 20:42:59 -0400

    Creando IclienteServices del Classlibrary tecnicos abstractions

---
+++ Tecnicos.Abstractions/IClienteService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tecnico.Domain.DTO;
+using Tecnicos.Data.Models;
+
+namespace Tecnicos.Abstractions;
+
+public interface IClienteService
+{
+    Task<bool> Guardar(ClienteDTO clienteDTO);
+    Task<bool> Eliminar(int clienteId);
+    Task<ClienteDTO> Buscar(int id);
+    Task<List<ClienteDTO>> Listar(Expression<Func<ClienteDTO, bool>> criterio);
+    Task<bool> Existe(int clientesId);
+    Task<bool> ExiteNombres(string nombre, int? clienteId = null);
+}
