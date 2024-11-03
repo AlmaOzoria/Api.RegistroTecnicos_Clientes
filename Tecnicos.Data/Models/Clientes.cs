d74f584b2bc3591c7be9af4cf785f4b2c21cf3b1
Alma Ozoria <heisylior@hotmail.com>
2024-11-02 20:41:22 -0400

    creando clientes de la carpeta models

---
+++ Tecnicos.Data/Models/Clientes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Data.Models;
+
+public class Clientes
+{
+    [Key]
+
+    [Required(ErrorMessage = "El Campo Cliente es obligatorio")]
+    public int ClientesId { get; set; }
+
+    [Required(ErrorMessage = "El Campo Nombre es obligatorio")]
+    public string? Nombres { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El campo WhatsApp es obligatorio.")]
+    public string? WhatsApp { get; set; }
+}
