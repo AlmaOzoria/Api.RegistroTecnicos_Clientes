a454b9e52df8560d13cf092545ab6d2ee3977ac6
Alma Ozoria <heisylior@hotmail.com>
2024-11-03 23:03:57 -0400

    Modificando el program

--- Api.RegistroTecnicos/Program.cs
+++ Api.RegistroTecnicos/Program.cs
@@ -21,9 +21,10 @@ namespace Api.RegistroTecnicos
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+               
             }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
